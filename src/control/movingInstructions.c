0781223ec91c4f31eb9a4ead0477783420f4ce84
Nathan <naw21@aber.ac.uk>
2018-04-17 12:47:21 +0100

    tweaks

--- src/control/movingInstructions.c
+++ src/control/movingInstructions.c
@@ -4,10 +4,11 @@
 #include "../lib/allcode_api.h"
 #include "stdlib.h"
 
-#define CM15 (140*3) 
+#define CM15 (145*3) 
 #define TURNDURATION 155
 
 int goneForward = false;
+
 void gotoCellWest(struct RobotState * robotState){
     //turn the robot to west and drive forward
     FA_BTSendString ("WestAction\n", 12);
@@ -59,17 +60,15 @@ void turnThenStraight(int direction, struct RobotState * robotState){
 void turnA(int orientation, struct RobotState * robotState){
     int L = robotState->LEncoders;
     int R = robotState->REncoders;
-    FA_BTSendString ("Turn\n", 6);
-    if(L - robotState->prevLEncoder > TURNDURATION && R - robotState->prevREncoder > TURNDURATION ){
+    FA_BTSendString ("TurnA\n", 7);
+    if(L> TURNDURATION && R > TURNDURATION ){
         int i = (robotState->orientation + 1)%4;
         robotState->orientation = i;
-        robotState->prevLEncoder = 0;
-        robotState->prevREncoder = 0;
         robotState->LSpeed = 0;
         robotState->RSpeed = 0;
     } else {
-        robotState->LSpeed = 30;
-        robotState->RSpeed = -30;
+        robotState->LSpeed = 30  + robotState->LEncoders - robotState->REncoders;
+        robotState->RSpeed = -35;
     }
 }
 
@@ -87,7 +86,7 @@ void turn(int orientation, struct RobotState * robotState){
         robotState->RSpeed = 0;
     } else {
         robotState->LSpeed = -30;
-        robotState->RSpeed = 35;
+        robotState->RSpeed = 35 + robotState->REncoders - robotState->LEncoders;
     }
 }
 
@@ -105,24 +104,7 @@ void foreward(struct RobotState * robotState){
             robotState->RSpeed = 35;
         } else {
             //Go in a straight line.
-            if(robotState->LEncoders < robotState->REncoders){
-                robotState->RSpeed = robotState->RSpeed +  3;
-            } else if(robotState->LEncoders > robotState->REncoders) {
-                robotState->RSpeed = robotState->RSpeed - 3;
-            }
-            /**
-            int Lline = FA_ReadLine(CHANNEL_LEFT);
-            int Rline = FA_ReadLine(CHANNEL_RIGHT);
-            char message[30];
-            sprintf(message,"L_%d_%d\n",
-                Lline,Rline
-            );
-            FA_BTSendString(message,30);
-            if(Lline < 100 && Lline > Rline ){
-                robotState->LSpeed = robotState->LSpeed - 3;
-            } else if (Rline < 100 && Lline < Rline) {
-                robotState->LSpeed = robotState->LSpeed + 3;
-            }
+            robotState->RSpeed = 35 +  robotState->REncoders - robotState->LEncoders;
             /**/
         }  
     }
