039ed6d8b1a52f244894d515dd4130859b5d4002
Nathan <naw21@aber.ac.uk>
2018-04-23 18:07:08 +0100

    tweaks round 6

--- src/control/movingInstructions.c
+++ src/control/movingInstructions.c
@@ -4,8 +4,8 @@
 #include "../lib/allcode_api.h"
 #include <stdlib.h>
 
-#define CM15 430
-#define TURNDURATION 153
+#define CM15 425
+#define TURNDURATION 156
 #define INITIALL 30
 #define INITIALR 35
 
@@ -41,13 +41,13 @@ void gotoCellSouth(struct RobotState * robotState){
 
 void turnThenStraight(int direction, struct RobotState * robotState){
     if(robotState->orientation != direction) 
-        if((robotState->orientation+1)%4 == direction) turnA(direction,robotState);
-        else turn(direction,robotState); 
+        if((robotState->orientation+1)%4 == direction) turnRight(direction,robotState);
+        else turnLeft(direction,robotState); 
     else {
         if(goneForward != true){
             foreward(robotState);
         } else {
-            FA_DelaySecs(1); //big delay after each movement so chance to correct robot.
+            //FA_DelaySecs(1); //big delay after each movement so chance to correct robot.
             //must be done go to next instruction
             struct Instruction * i = robotState->instruction;
             robotState->instruction = robotState->instruction->nextInstruction;
@@ -59,11 +59,31 @@ void turnThenStraight(int direction, struct RobotState * robotState){
 
 
 
-void turnA(int orientation, struct RobotState * robotState){
+void turnRight(int orientation, struct RobotState * robotState){
     int L = robotState->LEncoders;
     int R = robotState->REncoders;
     //FA_BTSendString ("TurnA\n", 7);
     if(L> TURNDURATION && R > TURNDURATION ){
+        FA_SetMotors(0,0);
+        FA_DelayMillis(100);
+        while(L > TURNDURATION + 10 && R > TURNDURATION + 10){
+            FA_SetDriveSpeed(20);
+            FA_Left(3);
+            L = FA_ReadEncoder(CHANNEL_LEFT);
+            R = FA_ReadEncoder(CHANNEL_RIGHT);
+            char message[30];
+            sprintf(message,"L_%d_R_%d\n",
+                L,
+                R
+            );
+            FA_BTSendString(message,30);
+        }
+        while(L < TURNDURATION + 10 && R < TURNDURATION + 10 && L > TURNDURATION && R > TURNDURATION){
+            FA_SetDriveSpeed(20);
+            FA_Right(3);
+            L = FA_ReadEncoder(CHANNEL_LEFT);
+            R = FA_ReadEncoder(CHANNEL_RIGHT);
+        }
         int i = (robotState->orientation + 1)%4;
         robotState->orientation = i;
         robotState->LSpeed = 0;
@@ -74,11 +94,25 @@ void turnA(int orientation, struct RobotState * robotState){
     }
 }
 
-void turn(int orientation, struct RobotState * robotState){
+void turnLeft(int orientation, struct RobotState * robotState){
     int L = robotState->LEncoders;
     int R = robotState->REncoders;
     //FA_BTSendString ("Turn\n", 6);
     if(L > TURNDURATION && R > TURNDURATION ){
+        FA_SetMotors(0,0);
+        FA_DelayMillis(100);
+        while(L > TURNDURATION + 10 && R > TURNDURATION + 10){
+            FA_SetDriveSpeed(20);
+            FA_Right(3);
+            L = FA_ReadEncoder(CHANNEL_LEFT);
+            R = FA_ReadEncoder(CHANNEL_RIGHT);
+        }
+        while(L < TURNDURATION + 10 && R < TURNDURATION + 10 && L > TURNDURATION && R > TURNDURATION){
+            FA_SetDriveSpeed(20);
+            FA_Left(3);
+            L = FA_ReadEncoder(CHANNEL_LEFT);
+            R = FA_ReadEncoder(CHANNEL_RIGHT);
+        }
         int i = robotState->orientation - 1;
         if(i <0){
             i=3;
@@ -96,7 +130,7 @@ void foreward(struct RobotState * robotState){
     int L = robotState->LEncoders;
     int R = robotState->REncoders;
     //FA_BTSendString ("foreward\n", 10);
-    if( robotState->location->frontDistance < 30 || (L > CM15 && R > CM15) ){ //go for a set distance
+    if( robotState->location->frontDistance < 40 || (L > CM15 && R > CM15) ){ //go for a set distance
         robotState->LSpeed = 0;
         robotState->RSpeed = 0;
         goneForward = true;
@@ -106,18 +140,16 @@ void foreward(struct RobotState * robotState){
             robotState->RSpeed =  INITIALR;
         } else {
             //Go in a straight line.
-            if (robotState->location->frontDistance > 40) {
-                if (robotState->location->leftDistance < 27)             robotState->RSpeed = INITIALR - 6; 
-                else if(robotState->location->frontLeftDistance < 32)    robotState->RSpeed = INITIALR - 9; 
+            if (robotState->location->frontDistance > 40) { 
+                if (robotState->location->leftDistance < 30)             robotState->RSpeed = INITIALR - 6; 
+                else if(robotState->location->frontLeftDistance < 35)    robotState->RSpeed = INITIALR - 10; 
                 else                                                     robotState->RSpeed = INITIALR;
-            }
-            if (robotState->location->frontDistance > 40) {
-                if (robotState->location->rightDistance < 27)            robotState->LSpeed = INITIALL - 6; 
-                else if (robotState->location->frontRightDistance < 32)  robotState->LSpeed = INITIALL - 9;
+
+                if (robotState->location->rightDistance < 30)            robotState->LSpeed = INITIALL - 6; 
+                else if (robotState->location->frontRightDistance < 35)  robotState->LSpeed = INITIALL - 10;
                 else                                                     robotState->LSpeed = INITIALL;
             }
             robotState->RSpeed = robotState->RSpeed + robotState->LEncoders - robotState->REncoders;
-            /**/
         }  
     }
 }
--- src/control/movingInstructions.h
+++ src/control/movingInstructions.h
@@ -12,8 +12,8 @@ void gotoCellSouth(struct RobotState * robotState);
 
 
 //Helper states
-void turn(int orientation, struct RobotState * robotState);
-void turnA(int orientation, struct RobotState * robotState);
+void turnRight(int orientation, struct RobotState * robotState);
+void turnLeft(int orientation, struct RobotState * robotState);
 void foreward(struct RobotState * robotState);
 void turnThenStraight(int direction, struct RobotState * robotState);
 
